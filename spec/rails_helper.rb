8852f92de58d9f40d8d21d734f82cc328d592257
Ryan MacGillivray <rmacg89@gmail.com>
2016-11-20 20:53:25 +0000

    Rubocop cleanup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,8 @@ Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if
+  Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 
--- spec/support/load_factories.rb
+++ spec/support/load_factories.rb
@@ -7,4 +7,3 @@ FactoryGirl.find_definitions
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
-
